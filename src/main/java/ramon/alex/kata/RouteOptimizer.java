a68d8f0f4b407097a45a5f0c1f066e0559573dba
ramonhaefliger <ramon.haefliger@icloud.com>
2023-05-26 10:58:56 +0200

    setup test to calculate combinations

--- src/main/java/ramon/alex/kata/RouteOptimizer.java
+++ src/main/java/ramon/alex/kata/RouteOptimizer.java
@@ -1,8 +1,14 @@
 package ramon.alex.kata;
 
+import java.util.List;
+
 public class RouteOptimizer {
 
-    public int choose_best_sum(int maxDistance, int nrOfCities, int[] distances ){
+    public int chooseBestSum(int maxDistance, int nrOfCities, int[] distances){
         return 0;
+    }
+
+    public List<int[]> calculateAllPossibleCombinations(int nrOfCities, int[] distances){
+        return null;
     }
 }
--- src/test/java/ramon/alex/kata/RouteOptimizerTest.groovy
+++ src/test/java/ramon/alex/kata/RouteOptimizerTest.groovy
@@ -4,16 +4,33 @@ import spock.lang.Specification
 
 class RouteOptimizerTest extends Specification {
 
-    def "Choose_best_sum"() {
+    def "test to illustrate spock"() {
         given:
-        var a = 7
-        var b = 3
+        def a = 7
+        def b = 3
 
         when:
-        var c = a + b
+        def c = a + b
 
         then:
         c == 10
 
     }
+
+    def "ensure all possible array combinations are calculated"() {
+        given:
+        def routeOptimizer = new RouteOptimizer()
+        def nrOfCities = 3
+        int[] distances = [50, 55, 57, 58, 60]
+        List<int[]> expectedCombinations = [[50,55,57],[50,55,58],[50,55,60],[50,57,58],[50,57,60],[50,58,60],[55,57,58],[55,57,60],[55,58,60],[57,58,60]]
+
+        when:
+        def combinations = routeOptimizer.calculateAllPossibleCombinations(nrOfCities, distances)
+
+        then:
+        combinations == expectedCombinations
+
+    }
+
+
 }
